fb3e48e0b8eb3c06be7c16eecca3ddc849084cc0
Your Name <you@example.com>
2026-04-13 21:54:14 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+COLOR_RESET=$(tput sgr0)
+COLOR_RED=$(tput setaf 1)
+COLOR_GREEN=$(tput setaf 2)
+COLOR_YELLOW=$(tput setaf 3)
+COLOR_BLUE=$(tput setaf 4)
+COLOR_PURPLE=$(tput setaf 5)
+COLOR_CYAN=$(tput setaf 6)
+
+echo -e "Welcome to the custom ${COLOR_CYAN}Arch Linux${COLOR_RESET} installation script from ${COLOR_CYAN}Noir${COLOR_RESET} user.\n"
