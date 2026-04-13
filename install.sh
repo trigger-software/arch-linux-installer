8ee1349be983778549c22c698011562c7f7daade
Your Name <you@example.com>
2026-04-13 22:11:04 +0300

    1 commit

--- install.sh
+++ install.sh
@@ -8,4 +8,10 @@ COLOR_BLUE=$(tput setaf 4)
 COLOR_PURPLE=$(tput setaf 5)
 COLOR_CYAN=$(tput setaf 6)
 
-echo -e "Welcome to the custom ${COLOR_CYAN}Arch Linux${COLOR_RESET} installation script from ${COLOR_CYAN}Noir${COLOR_RESET} user.\n"
+echo "Welcome to the custom ${COLOR_CYAN}Arch Linux${COLOR_RESET} installation script from ${COLOR_CYAN}Noir${COLOR_RESET} user.\n"
+
+echo "${RED_COLOR}WARNING! This script may not work correctly on your system.${COLOR_RESET}\n"
+echo "${COLOR_YELLOW}When running this script, ${COLOR_RED}you must understand that it does not guarantee the correct installation of Arch Linux on your machine.${COLOR_RESET}\n"
+echo "${COLOR_YELLOW}It can also ${COLOR_RED}disrupt your disk layout, as well as erase data from it during execution${COLOR_YELLOW}, in this case you will have to install another system on your machine.${COLOR_RESET}\n"
+echo "${COLOR_YELLOW}Be careful and make sure you are confident before running this script.${COLOR_RESET}\n\n"
+echo "${GREEN_COLOR}Good luck!${COLOR_RESET}"
